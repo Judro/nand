57890645ef5ad63c0174661900555348601a301e
Felix Drodofsky <Drodofsky@protonmail.com>
2023-09-14 15:37:05 +0200

    switch to nom-locate2

--- hdl-parser/src/error.rs
+++ hdl-parser/src/error.rs
@@ -1,10 +1,10 @@
 use miette::{Diagnostic, NamedSource, SourceSpan};
 use thiserror::Error;
 
-#[derive(Error,Debug,Diagnostic)]
+#[derive(Error, Debug, Diagnostic)]
 #[error("HDL Error")]
-pub struct HdlError{
+pub struct HdlError {
     #[source_code]
-    src:NamedSource,
-    error_pos: SourceSpan 
+    src: NamedSource,
+    error_pos: SourceSpan,
 }
--- hdl-parser/src/lib.rs
+++ hdl-parser/src/lib.rs
@@ -9,20 +9,23 @@ use nom::{
     sequence::{delimited, pair, preceded, separated_pair, terminated, tuple},
     IResult,
 };
-use nom_locate::{locate, Located};
-use nom_supreme::{error::{ErrorTree, GenericErrorTree}, final_parser::final_parser};
+use nom_locate2::*;
+use nom_supreme::{
+    error::{ErrorTree, GenericErrorTree},
+    final_parser::final_parser,
+};
 
-pub type SResult<I, O> = IResult<I, O, ErrorTree<I>>;
+pub type LResult<'s, O> = IResult<Input<'s>, O, ErrorTree<Input<'s>>>;
 
-pub type LChipName<'s> = Located<ChipName<'s>, &'s str>;
+pub type LChipName<'s> = Located<'s, ChipName<'s>>;
 
-pub type LPinName<'s> = Located<PinName<'s>, &'s str>;
+pub type LPinName<'s> = Located<'s, PinName<'s>>;
 
-pub type LRhsConnector<'s> = Located<RhsConnector<'s>, &'s str>;
+pub type LRhsConnector<'s> = Located<'s, RhsConnector<'s>>;
 
-pub type LLhsConnector<'s> = Located<LhsConnector<'s>, &'s str>;
+pub type LLhsConnector<'s> = Located<'s, LhsConnector<'s>>;
 
-pub type LUnsigned<'s> = Located<u32, &'s str>;
+pub type LUnsigned<'s> = Located<'s, u32>;
 
 #[derive(Debug)]
 pub struct Chip<'s> {
@@ -66,30 +69,38 @@ pub struct ConnectionList<'s>(Vec<Connection<'s>>);
 #[derive(Debug, PartialEq)]
 pub struct ChipName<'s>(&'s str);
 
+impl CharsLen for ChipName<'_> {
+    fn chars_len(&self) -> usize {
+        self.0.chars_len()
+    }
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Part<'s> {
     name: LChipName<'s>,
     connections: ConnectionList<'s>,
 }
 
-pub fn parse_hdl2(input: &str) ->Result<Chip,HdlError> {
-    match parse_hdl(input) {
+pub fn parse_hdl2(input: &str) -> Result<Chip, HdlError> {
+    match parse_hdl(Input::new(input)) {
         Ok(c) => Ok(c),
-        Err(GenericErrorTree::Base { location, kind })=>{
+        Err(GenericErrorTree::Base { location, kind }) => {
             panic!("can not convert location: {location}")
-        }Err(GenericErrorTree::Stack { base, contexts })=>{
+        }
+        Err(GenericErrorTree::Stack { base, contexts }) => {
             todo!()
-        }Err(GenericErrorTree::Alt(alts))=>{
+        }
+        Err(GenericErrorTree::Alt(alts)) => {
             todo!()
         }
     }
 }
 
-pub fn parse_hdl(input: &str) -> Result<Chip, ErrorTree<&str>> {
+pub fn parse_hdl(input: Input) -> Result<Chip, ErrorTree<Input>> {
     final_parser(chip)(input)
 }
 
-fn chip(input: &str) -> SResult<&str, Chip<'_>> {
+fn chip(input: Input) -> LResult<Chip<'_>> {
     map(pair(chip_head, chip_body), |(h, b)| Chip {
         name: h,
         in_decl: b.0,
@@ -98,25 +109,25 @@ fn chip(input: &str) -> SResult<&str, Chip<'_>> {
     })(input)
 }
 
-fn identifier(input: &str) -> SResult<&str, &str> {
-    preceded(
+fn identifier(input: Input) -> LResult<&str> {
+    get_str(preceded(
         not(digit1),
         take_while1(|c: char| c.is_alphanumeric() || c == '_'),
-    )(input)
+    ))(input)
 }
 
-fn unsigned(input: &str) -> SResult<&str, u32> {
-    map_res(digit1, |u: &str| u.parse::<u32>())(input)
+fn unsigned(input: Input) -> LResult<u32> {
+    map_res(get_str(digit1), |u: &str| u.parse::<u32>())(input)
 }
 
-pub fn chip_head(input: &str) -> SResult<&str, LChipName> {
+pub fn chip_head(input: Input) -> LResult<LChipName> {
     preceded(
         delimited(skip_white, tag("CHIP"), skip_white),
         locate(chip_name),
     )(input)
 }
 
-pub fn chip_body(input: &str) -> SResult<&str, (Vec<LPinName>, Vec<LPinName>, Vec<Part>)> {
+pub fn chip_body(input: Input) -> LResult<(Vec<LPinName>, Vec<LPinName>, Vec<Part>)> {
     map(
         delimited(
             preceded(skip_white, char('{')),
@@ -130,37 +141,37 @@ pub fn chip_body(input: &str) -> SResult<&str, (Vec<LPinName>, Vec<LPinName>, Ve
     )(input)
 }
 
-fn skip_white(input: &str) -> SResult<&str, &str> {
-    multispace0(input)
+fn skip_white(input: Input) -> LResult<&str> {
+    get_str(multispace0)(input)
 }
 
-fn pin_list(input: &str) -> SResult<&str, Vec<LPinName>> {
+fn pin_list(input: Input) -> LResult<Vec<LPinName>> {
     separated_list0(
         preceded(skip_white, char(',')),
         preceded(skip_white, locate(pin_name)),
     )(input)
 }
 
-fn pin_name(input: &str) -> SResult<&str, PinName> {
-    map(identifier, |s| PinName(s))(input)
+fn pin_name(input: Input) -> LResult<PinName> {
+    map(identifier, PinName)(input)
 }
 
-fn chip_name(input: &str) -> SResult<&str, ChipName> {
-    map(identifier, |s| ChipName(s))(input)
+fn chip_name(input: Input) -> LResult<ChipName> {
+    map(identifier, ChipName)(input)
 }
 
-fn pin_decl<P: Pin>(input: &str) -> SResult<&str, Vec<LPinName>> {
+fn pin_decl<P: Pin>(input: Input) -> LResult<Vec<LPinName>> {
     preceded(
         skip_white,
         delimited(tag(P::conn()), pin_list, preceded(skip_white, char(';'))),
     )(input)
 }
 
-fn bool(input: &str) -> SResult<&str, bool> {
+fn bool(input: Input) -> LResult<bool> {
     alt((map(tag("true"), |_| true), map(tag("false"), |_| false)))(input)
 }
 
-fn bus_slice(input: &str) -> SResult<&str, BusSlice> {
+fn bus_slice(input: Input) -> LResult<BusSlice> {
     map(
         pair(
             locate(pin_name),
@@ -181,7 +192,7 @@ fn bus_slice(input: &str) -> SResult<&str, BusSlice> {
     )(input)
 }
 
-fn rhs_connector(input: &str) -> SResult<&str, LRhsConnector> {
+fn rhs_connector(input: Input) -> LResult<LRhsConnector> {
     locate(alt((
         map(bool, RhsConnector::Potential),
         map(bus_slice, RhsConnector::Slice),
@@ -189,14 +200,14 @@ fn rhs_connector(input: &str) -> SResult<&str, LRhsConnector> {
     )))(input)
 }
 
-fn lhs_connector(input: &str) -> SResult<&str, LLhsConnector> {
+fn lhs_connector(input: Input) -> LResult<LLhsConnector> {
     locate(alt((
         map(bus_slice, LhsConnector::Slice),
         map(identifier, LhsConnector::Bus),
     )))(input)
 }
 
-fn connection(input: &str) -> SResult<&str, Connection> {
+fn connection(input: Input) -> LResult<Connection> {
     map(
         separated_pair(
             preceded(skip_white, lhs_connector),
@@ -207,14 +218,14 @@ fn connection(input: &str) -> SResult<&str, Connection> {
     )(input)
 }
 
-fn connection_list(input: &str) -> SResult<&str, ConnectionList> {
+fn connection_list(input: Input) -> LResult<ConnectionList> {
     map(
         separated_list0(preceded(skip_white, char(',')), connection),
-        |c| ConnectionList(c),
+        ConnectionList,
     )(input)
 }
 
-fn part(input: &str) -> SResult<&str, Part> {
+fn part(input: Input) -> LResult<Part> {
     map(
         terminated(
             pair(
@@ -258,59 +269,60 @@ mod tests {
     use super::*;
     #[test]
     fn ident_0() {
-        let (rem, res) = identifier("ALU_01!").unwrap();
-        assert_eq!(rem, "!");
+        let (rem, res) = identifier(Input::new("ALU_01!")).unwrap();
+        assert_eq!(rem.into_fragment(), "!");
         assert_eq!(res, "ALU_01");
     }
 
     #[test]
-    #[should_panic]
     fn ident_1() {
-        let (_rem, _res) = identifier("99ALU_01!").unwrap();
+        let r = identifier(Input::new("99ALU_01!"));
+        assert!(r.is_err())
     }
 
     #[test]
     fn ident_2() {
-        let (rem, res) = identifier("_ALU_01 ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, res) = identifier(Input::new("_ALU_01 ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(res, "_ALU_01");
     }
 
     #[test]
     fn chip_head_0() {
-        let (rem, res) = chip_head("\tCHIP ALU_01!").unwrap();
-        assert_eq!(rem, "!");
+        let (rem, res) = chip_head(Input::new("\tCHIP ALU_01!")).unwrap();
+        assert_eq!(rem.into_fragment(), "!");
         assert_eq!(res, ChipName("ALU_01"));
+        assert_eq!(res.span(), (6, 11))
     }
     #[test]
-    #[should_panic]
     fn chip_head_1() {
-        let (_rem, _res) = chip_head("\tChip ALU_01!").unwrap();
+        let res = chip_head(Input::new("\tChip ALU_01!"));
+        assert!(res.is_err())
     }
 
     #[test]
     fn chip_head_2() {
-        let (rem, res) = chip_head("CHIPALU_01!").unwrap();
-        assert_eq!(rem, "!");
+        let (rem, res) = chip_head(Input::new("CHIPALU_01!")).unwrap();
+        assert_eq!(rem.into_fragment(), "!");
         assert_eq!(res, ChipName("ALU_01"));
     }
 
     #[test]
     fn body_0() {
-        let (rem, _res) = chip_body(" {\tIN;\nOUT; PARTS: } ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, _res) = chip_body(Input::new(" {\tIN;\nOUT; PARTS: } ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
     }
 
     #[test]
     #[should_panic]
     fn body_1() {
-        let (_rem, _res) = chip_body(" {  ").unwrap();
+        let (_rem, _res) = chip_body(Input::new(" {  ")).unwrap();
     }
 
     #[test]
     fn pin_list_0() {
-        let (rem, res) = pin_list("a, b , c,d;").unwrap();
-        assert_eq!(rem, ";");
+        let (rem, res) = pin_list(Input::new("a, b , c,d;")).unwrap();
+        assert_eq!(rem.into_fragment(), ";");
         assert_eq!(
             res,
             vec![PinName("a"), PinName("b"), PinName("c"), PinName("d")]
@@ -319,15 +331,15 @@ mod tests {
 
     #[test]
     fn pin_list_1() {
-        let (rem, res) = pin_list("a, b  c,d;").unwrap();
+        let (rem, res) = pin_list(Input::new("a, b  c,d;")).unwrap();
         assert_eq!(res, vec![PinName("a"), PinName("b")]);
-        assert_eq!(rem, "  c,d;");
+        assert_eq!(rem.into_fragment(), "  c,d;");
     }
 
     #[test]
     fn input_pin_0() {
-        let (rem, res) = pin_decl::<In>("IN a, b , c,d; ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, res) = pin_decl::<In>(Input::new("IN a, b , c,d; ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(
             res,
             vec![PinName("a"), PinName("b"), PinName("c"), PinName("d")]
@@ -337,19 +349,19 @@ mod tests {
     #[test]
     #[should_panic]
     fn input_pin_1() {
-        let (_rem, _res) = pin_decl::<In>("IN a, b , c,d").unwrap();
+        let (_rem, _res) = pin_decl::<In>(Input::new("IN a, b , c,d")).unwrap();
     }
 
     #[test]
     #[should_panic]
     fn input_pin_2() {
-        let (_rem, _res) = pin_decl::<In>("OUT a, b , c,d").unwrap();
+        let (_rem, _res) = pin_decl::<In>(Input::new("OUT a, b , c,d")).unwrap();
     }
 
     #[test]
     fn output_pin_0() {
-        let (rem, res) = pin_decl::<Out>("OUT a, b , c,d; ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, res) = pin_decl::<Out>(Input::new("OUT a, b , c,d; ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(
             res,
             vec![PinName("a"), PinName("b"), PinName("c"), PinName("d")]
@@ -358,8 +370,8 @@ mod tests {
 
     #[test]
     fn bus_slice0() {
-        let (rem, slice) = bus_slice("a [ 22 ] ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, slice) = bus_slice(Input::new("a [ 22 ] ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(slice.name, PinName("a"));
         assert_eq!(slice.slice.0, 22);
         assert_eq!(slice.slice.1, 22);
@@ -367,8 +379,8 @@ mod tests {
 
     #[test]
     fn bus_slice1() {
-        let (rem, slice) = bus_slice("a[ 1 .. 3 ] ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, slice) = bus_slice(Input::new("a[ 1 .. 3 ] ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(slice.name, PinName("a"));
         assert_eq!(slice.slice.0, 1);
         assert_eq!(slice.slice.1, 3);
@@ -376,49 +388,50 @@ mod tests {
 
     #[test]
     fn rhs_connector_0() {
-        let (rem, res) = rhs_connector("true ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, res) = rhs_connector(Input::new("true ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(res, RhsConnector::Potential(true));
     }
 
     #[test]
     fn rhs_connector_1() {
-        let (rem, res) = rhs_connector("false ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, res) = rhs_connector(Input::new("false ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(res, RhsConnector::Potential(false));
     }
 
     #[test]
     fn rhs_connector_2() {
-        let (rem, res) = rhs_connector("ab ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, res) = rhs_connector(Input::new("ab ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(res, RhsConnector::Bus("ab"));
     }
 
     #[test]
     fn rhs_connector_3() {
-        let (rem, _res) = rhs_connector("a[10..12] ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, _res) = rhs_connector(Input::new("a[10..12] ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         // just check that it works
     }
 
     #[test]
     fn connection_0() {
-        let (rem, Connection { lhs: a, rhs: b }) = connection(" a = b ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, Connection { lhs: a, rhs: b }) = connection(Input::new(" a = b ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(a, LhsConnector::Bus("a"));
         assert_eq!(b, RhsConnector::Bus("b"));
     }
     #[test]
     #[should_panic]
     fn connection_1() {
-        let (_rem, _c) = connection(" a = 1 ").unwrap();
+        let (_rem, _c) = connection(Input::new(" a = 1 ")).unwrap();
     }
 
     #[test]
     fn connection_list_0() {
-        let (rem, ConnectionList(list)) = connection_list(" a = b , c = false , d= in ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, ConnectionList(list)) =
+            connection_list(Input::new(" a = b , c = false , d= in ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(list[0].lhs.get(), &LhsConnector::Bus("a"));
         assert_eq!(list[1].lhs.get(), &LhsConnector::Bus("c"));
         assert_eq!(list[2].lhs.get(), &LhsConnector::Bus("d"));
@@ -426,16 +439,16 @@ mod tests {
 
     #[test]
     fn part_0() {
-        let (rem, part) = part("And ( a = a , b = false ) ; ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, part) = part(Input::new("And ( a = a , b = false ) ; ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(part.name, ChipName("And"));
         assert_eq!(part.connections.0.len(), 2);
     }
 
     #[test]
     fn chip_0() {
-        let (rem, chip) = chip("CHIP And { \n IN a,b; OUT out; \n PARTS: \n Nand(a=a,b=b,out=c); \n Not(in=c,out=out); } ").unwrap();
-        assert_eq!(rem, " ");
+        let (rem, chip) = chip(Input::new("CHIP And { \n IN a,b; OUT out; \n PARTS: \n Nand(a=a,b=b,out=c); \n Not(in=c,out=out); } ")).unwrap();
+        assert_eq!(rem.into_fragment(), " ");
         assert_eq!(chip.name, ChipName("And"));
         assert_eq!(chip.in_decl, vec![PinName("a"), PinName("b")]);
         assert_eq!(chip.out_decl, vec![PinName("out")]);
